344ad189d5f5c40fa31b20c7f99924f449e4b178
FuikenTea <guspenrod@gmail.com>
2023-11-20 16:52:43 +0000

    fixed nav back to products

--- angular-app/src/app/core/components/nav.component.ts
+++ angular-app/src/app/core/components/nav.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
       <p class="menu-label">Menu</p>
       <ul class="menu-list">
         <a routerLink="/Products" routerLinkActive="router-link-active">
-          <span>Teams</span>
+          <span>Products</span>
         </a>
         <a routerLink="/about" routerLinkActive="router-link-active">
           <span>About</span>
