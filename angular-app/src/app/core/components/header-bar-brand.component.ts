f92db0072fb77918239abd33ad1342e1e6f5a815
FuikenTea <guspenrod@gmail.com>
2023-11-20 17:07:50 +0000

    nav fix

--- angular-app/src/app/core/components/header-bar-brand.component.ts
+++ angular-app/src/app/core/components/header-bar-brand.component.ts
@@ -13,7 +13,7 @@ import { Component } from '@angular/core';
         <i class="fab js-logo fa-angular fa-2x" aria-hidden="true"></i>
       </a>
       <a class="navbar-item nav-home" router-link="/">
-        <span class="brand-first">Welcome to &#32 </span>
+        <span class="brand-first">Welcome to </span>
         <span class="brand-second">BATTLE </span>
         <span class="brand-third">FRONTIER</span>
       </a>
