764c8fcaa2176ebd1c4327749bd071e83695cdb8
FuikenTea <guspenrod@gmail.com>
2023-11-20 16:41:06 +0000

    More nav updates

--- angular-app/src/app/core/components/nav.component.ts
+++ angular-app/src/app/core/components/nav.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     <nav class="menu">
       <p class="menu-label">Menu</p>
       <ul class="menu-list">
-        <a routerLink="/Teams" routerLinkActive="router-link-active">
+        <a routerLink="/Products" routerLinkActive="router-link-active">
           <span>Teams</span>
         </a>
         <a routerLink="/about" routerLinkActive="router-link-active">
