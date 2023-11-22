3c9bdd9ff1d7d93a972fcd43bf1187bba87656fc
FuikenTea <guspenrod@gmail.com>
2023-11-22 16:17:54 +0000

    navheader

--- angular-app/src/app/core/components/nav.component.ts
+++ angular-app/src/app/core/components/nav.component.ts
@@ -38,7 +38,7 @@ import { UserInfo } from '../model/user-info';
 export class NavComponent implements OnInit {
 
   userInfo: UserInfo;
-  providers = [ 'github', 'google', 'aad'];
+  providers = [ 'github', 'aad'];
   redirect = window.location.pathname;
 
   async ngOnInit() {
